246d491454cef7b0e3d9de31a23ce28314559f3e
Sergey Glazov <yaglazov@gmail.com>
2018-11-13 01:15:02 +0300

    initial demo

--- app/scripts/addwors.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#new-keyword').keyup(function(){
-    console.log('fgdfg');
-    if($(this).val().length !=0)
-      $('.add-word .i-plus-in-circle').addClass('blue');
-    else
-      $('.add-word .i-plus-in-circle').removeClass('blue');
-  });
-});
